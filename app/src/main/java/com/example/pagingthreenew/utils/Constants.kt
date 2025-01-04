ac4d9569b06c41f0bc8d55c05eb12aedf8a0b109
Yuliya Ahmatova <ahmatovay@mail.ru>
2025-01-04 14:20:51 +0500

    класс Constants

---
+++ app/src/main/java/com/example/pagingthreenew/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.pagingthreenew.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "АпиКлюч"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+    }
+}
